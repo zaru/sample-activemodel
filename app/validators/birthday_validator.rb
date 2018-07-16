084c3cf6377ef1cb89d547e8561090b874799d22
zaru@sakuraba <zarutofu@gmail.com>
2018-07-16 16:08:30 +0900

    don't check when birthday is nil

--- app/validators/birthday_validator.rb
+++ app/validators/birthday_validator.rb
@@ -1,5 +1,6 @@
 class BirthdayValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
+    return unless value.present?
     unless value.between? Date.parse('2017-01-01'), Date.parse('2017-12-31')
       record.errors[attribute] << '2017年生まれしかのみ'
     end
